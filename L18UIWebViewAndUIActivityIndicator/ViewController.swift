4a60e609182742e3f0779ab3917c256fa1dc0a0c
GRIGORIY ALEKSEEV <grin4500@mail.ru>
2020-10-29 22:56:21 +0300

    Initial Commit

---
+++ L18UIWebViewAndUIActivityIndicator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L18UIWebViewAndUIActivityIndicator
+//
+//  Created by Григорий on 29.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
